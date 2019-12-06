14789858892b741a773586e819d0e40da9c18f1f
DV54321 <danielviola4458@gmail.com>
2019-12-06 11:43:18 -0500

    First Commit

---
+++ program.ts
@@ -0,0 +1,34 @@
+console.log('Welcome to the Name Picker.')
+
+let firstName: string[] = [ 'Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+ "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite' ",
+ 'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+ 'Chewy', 'Chigger", "Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+ 'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+ 'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+ 'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+ 'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"',
+ 'Mergatroid', '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch',
+ 'Ovaltine', 'Pennywhistle', 'Pitchfork Ben', 'Potato Bug',
+ 'Pushmeet','Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+ "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+ 'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+ 'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+ "Winston 'Jazz Hands'", 'Worms' ]
+ let lastName: string[] = ['Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+ 'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+ 'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+ 'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+ 'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+ 'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+ 'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+ 'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+ 'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+ 'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+ 'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+ 'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+ 'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+ 'Woolysocks']
+ let randomFirst: any = firstName[Math.floor(Math.random() * firstName.length)]
+ let randomLast: any = lastName[Math.floor(Math.random() * lastName.length)]
+ console.log('Your silly name is ' + randomFirst + ' ' + randomLast)
